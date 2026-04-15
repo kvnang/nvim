10f8c04c27c461096bec09d17fc10d95c9f36fc3
Kevin Ang <ka@kevinang.com>
2026-04-15 10:39:03 -0400

    wip

---
+++ lua/plugins/snacks_explorer.lua
@@ -0,0 +1,33 @@
+return {
+  desc = "Snacks File Explorer",
+  recommended = true,
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      sources = {
+        explorer = {
+          hidden = true,
+          ignored = false,
+        },
+      },
+    },
+  },
+  keys = {
+    {
+      "<leader>fe",
+      function()
+        Snacks.explorer({ cwd = require("lazyvim.util").root() })
+      end,
+      desc = "Explorer Snacks (root dir)",
+    },
+    {
+      "<leader>fE",
+      function()
+        Snacks.explorer()
+      end,
+      desc = "Explorer Snacks (cwd)",
+    },
+    { "<leader>e", "<leader>fe", desc = "Explorer Snacks (root dir)", remap = true },
+    { "<leader>E", "<leader>fE", desc = "Explorer Snacks (cwd)", remap = true },
+  },
+}
